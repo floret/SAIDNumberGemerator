619c3b446ac0285ebe659ce1318b74f2265d85e3
floret <sorrowbedamned@gmail.com>
2013-12-04 01:42:40 +0200

    first 7 digits work.

--- IDNumberGenerator/IDNumberGenerator/frmID.Designer.cs
+++ IDNumberGenerator/IDNumberGenerator/frmID.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace IDNumberGenerator
 {
-    partial class Form1
+    partial class frmID
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.rtbOutpuut = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Location = new System.Drawing.Point(188, 110);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(154, 72);
+            this.btnGenerate.TabIndex = 0;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // rtbOutpuut
+            // 
+            this.rtbOutpuut.Location = new System.Drawing.Point(66, 226);
+            this.rtbOutpuut.Name = "rtbOutpuut";
+            this.rtbOutpuut.Size = new System.Drawing.Size(408, 212);
+            this.rtbOutpuut.TabIndex = 2;
+            this.rtbOutpuut.Text = "";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(566, 475);
+            this.Controls.Add(this.rtbOutpuut);
+            this.Controls.Add(this.btnGenerate);
             this.Name = "Form1";
-            this.Text = "o";
+            this.Text = "ID Number \"Gemerator\"";
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.RichTextBox rtbOutpuut;
     }
 }
 
---
+++ IDNumberGenerator/IDNumberGenerator/frmID.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IDNumberGenerator
+{
+    public partial class frmID : Form
+    {
+        public frmID()
+        {
+            InitializeComponent();
+        }
+        int[] arrID100 = new int[100];//used for storing the current  100 ID's.
+        Random rand1 = new Random();
+        Random rand2 = new Random();
+        Random rand3 = new Random();
+        Random rand4 = new Random();
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int dig1 = 0, dig2 = 0, dig3 = 0, dig7 = 0;
+            string sdig2 = "", sdig3 = "";
+            //creates digit 1 & 2
+            while (dig1 == 0 || dig1 > 90 || dig1 < 50)
+            {
+                dig1 = rand1.Next(100);
+            }
+            //creates digit 3 & 4           
+            dig2 = rand2.Next(13);
+            sdig2 = dig2.ToString();
+            if (dig2 < 10)
+            {
+                sdig2 = "0" + dig2.ToString();
+            }
+            //creates digit 5 & 6            
+            dig3 = rand3.Next(31);
+            if (dig2 == 2)//in case of february which has 28 days.
+            {
+                dig3 = rand3.Next(28);
+            }
+            sdig3 = dig3.ToString();
+            if (dig3 < 10)
+            {
+                sdig3 = "0" + dig3.ToString();
+            }
+            //
+            dig7 = rand4.Next(10);
+
+
+            rtbOutpuut.Text = dig1.ToString() + " " + sdig2 + " " + sdig3 + " " + dig7.ToString();
+        }
+    }
+}
